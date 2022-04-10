c77deb68021718c769acf7ebe893988addfd7a98
risingrj27 <rjprince18@gmail.com>
2022-04-11 00:05:50 +0530

    10-04-2022

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,20 +1,10 @@
+import ExpenseItem  from './component/expenseItem'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is my first compnent</h1>
+      <ExpenseItem />
     </div>
   );
 }
---
+++ todo/src/component/expenseItem.js
@@ -0,0 +1,7 @@
+function ExpenseItem() {
+    return <>
+       <h1> this is expense Item</h1>
+    </>
+}
+
+export default ExpenseItem;
