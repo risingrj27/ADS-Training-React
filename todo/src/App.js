c801bc28bc2bd3d93bea36598ea041c6fdbfd90e
risingrj27 <rjprince18@gmail.com>
2022-04-11 15:37:47 +0530

    11-04-2022

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,8 +2,7 @@ import ExpenseItem  from './component/expenseItem'
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is my first compnent</h1>
+    <div>
       <ExpenseItem />
     </div>
   );
